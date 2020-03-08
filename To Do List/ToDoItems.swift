b341a3bc73c81068856cbca564be88c31d03ef57
Heesu Yun <Yunheesu@icloud.com>
2020-03-08 00:58:57 -0500

    Section 5.15 - Reusable Code with a helper struct + static methods
    //section 5.13 was committed after 5.14 by undoing it because I forgot to commit  before committing 5.14 :( pls take into consideration

--- To Do List/ToDoItems.swift
+++ To Do List/ToDoItems.swift
@@ -7,22 +7,11 @@
 //
 
 import Foundation
+import UserNotifications
 
 class ToDoItems {
     var itemsArray: [ToDoItem] = []
     
-    func savaData() {
-        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask ).first!
-         let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
-         
-         let jsonEncoder = JSONEncoder ()
-         let data = try? jsonEncoder.encode(itemsArray)
-         do {
-             try data?.write(to: documentURL, options: .noFileProtection)
-         } catch{
-             print("😡 ERROR: Could not save data \(error.localizedDescription)")
-         }
-    }
     func loadData(completed: @escaping ()->()) {
          let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask ).first!
             let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
@@ -38,4 +27,35 @@ class ToDoItems {
          }
         completed()
      }
+    
+    func savaData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask ).first!
+         let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+         
+         let jsonEncoder = JSONEncoder ()
+         let data = try? jsonEncoder.encode(itemsArray)
+         do {
+             try data?.write(to: documentURL, options: .noFileProtection)
+         } catch{
+             print("😡 ERROR: Could not save data \(error.localizedDescription)")
+         }
+         setNotifications()
+    }
+    
+    func setNotifications() {
+        guard itemsArray.count > 0 else {
+            return
+        }
+        //remove all notification
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        //and let's re-create them with the updated data that we just saved
+        for index in 0..<itemsArray.count {
+            if itemsArray[index].reminderSet {
+                let toDoItem = itemsArray[index]
+                itemsArray[index].notificationID = LocalNotificationManager.setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
+        }
+    }
+    
 }
--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -27,72 +27,11 @@ class ToDoListViewController: UIViewController {
         toDoItems.loadData {
             self.tableView.reloadData()
         }
-        authorizeLocalNotification()
+        LocalNotificationManager.authorizeLocalNotifications()
          }
-    
-    func authorizeLocalNotification() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
-            guard error == nil else {
-                print("😡 ERROR: \(error!.localizedDescription)")
-                return
-            }
-            if granted {
-                print("✅ Notifications Authorization Granted!")
-            }else{
-                print("🚫The use has denied notifications!")
-                //TODO: Put an alert in here telling the use what to do
-            }
-        }
-    }
-    
-    func setNotifications() {
-        guard toDoItems.itemsArray.count > 0 else {
-            return
-        }
-        //remove all notification
-        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-        
-        //and let's re-create them with the updated data that we just saved
-        for index in 0..<toDoItems.itemsArray.count {
-            if toDoItems.itemsArray[index].reminderSet {
-                let toDoItem = toDoItems.itemsArray[index]
-                toDoItems.itemsArray[index].notificationID = setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
-            }
-        }
-    }
-    
-    func setCalendarNotification(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String {
-        //create content:
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.subtitle = subtitle
-        content.body = body
-        content.sound = sound
-        content.badge = badgeNumber
-        
-        //create trigger
-        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
-        dateComponents.second = 00
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-        
-        //create request
-        let notificationID = UUID().uuidString
-        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
-        
-        //register request with the notification center
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if let error = error {
-                print("😡ERROR: \(error.localizedDescription) Yikes, adding notification request went wrong!")
-            }else{
-                print("Notification scheduled \(notificationID), title:\(content.title)")
-                }
-        }
-        return notificationID
-    }
          
         func saveData() {
             toDoItems.savaData()
-            setNotifications()
             }
       
 
